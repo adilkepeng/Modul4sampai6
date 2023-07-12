f5e01abacdee595fc5064b41e36b7ef589a35641
Adil <adilfirdausy@gmail.com>
2023-07-12 12:23:22 +0700

    nyoba

---
+++ storage/framework/views/a5c1a92a1ccdec6cf49b8ea31d2363cd.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('layouts.default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Semester 4\Pemrograman Framework\LaravelPraktikumModul4-main\resources\views/home.blade.php ENDPATH**/ ?>
